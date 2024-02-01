4d923f48a2673cffb98fbe62392eee4c77968ff3
mauryavalambhiya <mvalambhiya737@rku.ac.in>
2024-02-01 13:52:02 +0530

    server running code added

---
+++ v1/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const server = express();
+const PORT = process.env.PORT || 5000
+
+server.disable("x-powered-by");
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// === START UP SERVER ===
+server.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+);
+
